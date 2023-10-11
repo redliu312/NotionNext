1997bee33cd39839dad214f4527b85ad75257629
redliu312 <tommyandrora@gmail.com>
2023-10-11 14:29:43 +0800

    Update NavPostListEmpty.js

--- themes/gitbook/components/NavPostListEmpty.js
+++ themes/gitbook/components/NavPostListEmpty.js
@@ -6,7 +6,7 @@
  */
 const NavPostListEmpty = ({ currentSearch }) => {
   return <div className='flex w-full items-center justify-center min-h-screen mx-auto md:-mt-20'>
-        <p className='text-gray-500 dark:text-gray-300'>没有找到文章 {(currentSearch && <div>{currentSearch}</div>)}</p>
+        <p className='text-gray-500 dark:text-gray-300'>not found {(currentSearch && <div>{currentSearch}</div>)}</p>
   </div>
 }
 export default NavPostListEmpty
