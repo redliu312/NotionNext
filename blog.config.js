2bfaa2673aa01e90b863ae536850aa96d416fe53
Liu Yen Hung <tommyandrora@gmail.com>
2023-12-03 16:13:46 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,8 +18,8 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'redliu', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'red, run, coding', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://redrun.dev', // 网站地址
+  BIO: 'Passionate runner and coding enthusiast.', // 作者简介
+  LINK: 'https://redrun.dev', // 网站地址
   KEYWORDS: 'red,running,coding', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
